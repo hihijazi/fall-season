c71db4ff069d13ebd404dc82fb5a1fd5a0494540
hadil <hihijazi@gmail.com>
2023-11-02 05:46:35 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,3 @@
+const h2 = document.createElement("h2");
+h2.textContent = "It's that time of year when it's too cold for Spring but still warmer than Winter.";
+document.querySelector("body").appendChild(h2);
